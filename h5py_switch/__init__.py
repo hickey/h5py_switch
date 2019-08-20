0dce86f7e136b24caf1a507b74234c8a495739eb
Aleksandar Jelenak <ajelenak@gharial.fios-router.home>
2019-08-20 17:36:03 -0400

    Fix bugs

--- h5py_switch/__init__.py
+++ h5py_switch/__init__.py
@@ -13,20 +13,15 @@ else:
     no_h5pyd = False
 
 
-__version__ = '0.0.1'
+def File(path, mode='a', **kwargs):
+    """Either h5py.File or h5pyd.File depending on path."""
+    if isinstance(path, str):
+        if path.startswith(('http://', 'https://', 'hdf5://')):
+            if no_h5pyd:
+                raise ImportError(
+                    'h5pyd package is required for: {}'.format(path))
+            return h5pyd.File(path, mode, **kwargs)
 
-
-class File():
-    """Seamless switch between h5py.File or h5pyd.File classes"""
-
-    def __init__(self, path, mode='a', **kwargs):
-        if isinstance(path, str):
-            if path.startswith(('http://', 'https://', 'hdf5://')):
-                if no_h5pyd:
-                    raise ImportError(
-                        'h5pyd package is required for: {}'.format(path))
-                return h5pyd.File(path, mode, **kwargs)
-
-        if no_h5py:
-            raise ImportError('h5py package is required for {}'.format(path))
-        return h5py.File(path, mode, **kwargs)
+    if no_h5py:
+        raise ImportError('h5py package is required for {}'.format(path))
+    return h5py.File(path, mode, **kwargs)
